c3914ff0cdf32dcd7b27fae0f2062f26f33fb936
Tom King <design.tomking@gmail.com>
2018-09-09 15:52:29 -0600

    Add footer component
    Add footer test file

---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const Footer = () => {
+  return (
+    <div>
+      <Link to='/profile'>( Profile )</Link>
+    </div>
+  )
+}
