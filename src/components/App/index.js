a5e63fd8d5beed7e85960f5fea813e4a4f703857
Tom King <design.tomking@gmail.com>
2018-09-09 15:50:49 -0600

    Add fetch,  navroute and pagecontentroute to app
    Add test file for app

---
+++ src/components/App/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import { PageContentRoute } from '../PageContentRoute';
+import { NavRoute } from '../NavRoute';
+import Fetch from '../../containers/Fetch';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Fetch />
+        <NavRoute />
+        <PageContentRoute />
+      </div>
+    );
+  }
+}
+
+export default App;
