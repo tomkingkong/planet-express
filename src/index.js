7385655b90fdf363352613a31e05b9291f659cea
Tom King <design.tomking@gmail.com>
2018-09-09 15:48:52 -0600

    Add router and redux to app

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+import { rootReducer } from './reducers';
+import './index.css';
+import App from './components/App/';
+import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+  </Provider>, document.getElementById('root'));
+registerServiceWorker();
