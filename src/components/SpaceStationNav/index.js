8f494e451fa53ca7af96dfc24fd17750cdc73f4f
Tom King <design.tomking@gmail.com>
2018-09-12 18:43:14 -0600

    Create a space station ui with destination and spacelab buttons

--- src/components/SpaceStationNav/index.js
+++ src/components/SpaceStationNav/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './SpaceStation.css';
+
 export const SpaceStationNav = () => {
+  const stationImgStyle = {
+    backgroundImage: "url(/images/station/ISS_overEarth_1.jpg)"
+  }
   return (
-    <div>
-      <p>Welcome to the ISS</p>
-      <Link to='/ISS/destinations'>Take a trip! >></Link><br/>
-      <Link to='/ISS/store'>Visit the Store >></Link>
+    <div className="SpaceStation">
+      <div className="station__img" style={stationImgStyle}/>
+      <h4 className="station__title">Welcome to the International Space Station</h4>
+      <Link className="station__dest" to='/ISS/destinations'>Take a trip!</Link>
+      <Link className="station__lab" to='/ISS/store'>Visit SpaceLab</Link>
     </div>
   )
 }
