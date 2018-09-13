a0199f47d12dfab0970df8ab483f7928d30a4ae1
Tom King <design.tomking@gmail.com>
2018-09-12 18:46:33 -0600

    Move api url info to separate file

---
+++ src/utilities/api/API.js
@@ -0,0 +1,6 @@
+export const API = {
+  exoplanets: 'https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI',
+  scienceNews: 'https://newsapi.org/v2/top-headlines?category=science&country=us&apiKey=',
+  nasaTech: 'https://techport.nasa.gov/api/projects',
+  hubble: 'http://hubblesite.org/api/v3/'
+}
