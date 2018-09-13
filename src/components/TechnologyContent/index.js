8afaa94a3e5ba4371c3993d92f43a82128866ac7
Tom King <design.tomking@gmail.com>
2018-09-12 18:43:50 -0600

    Create a tech content component to display spacelab dat

--- src/components/TechnologyContent/index.js
+++ src/components/TechnologyContent/index.js
@@ -1,31 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './Technology.css';
+
 export const TechnologyContent = ({ 
   title, 
-  discoveryMethod, 
   benefits, 
   org, 
-  director, 
   description, 
   techAreas, 
   status }) => {
-    const techTags = techAreas.map((tag, i) => <li key={tag+i}>{tag}</li>);
-    // const benefitList = benefits.map((benefit, i) => <li key={benefit+i}>{benefit}</li>);
+  let newTags = techAreas[0].split(',')
+  const techTags = newTags
+    .map((tag, i) => (<li className="tech__tag" key={tag+i}>{tag}</li>));
+  // const benefitList = benefits
+  //   .map((benefit, i) => <li key={benefit+i}>{benefit}</li>);
   return (
-    <article className="technology">
-      <h4>{title}</h4>
-      <p>{description}</p>
-      <p>{discoveryMethod}</p>
-      <p>{status}</p>
+    <article className="Technology">
+      <h4 className="tech__title">{title}</h4>
+      <p className="tech__desc">{description}</p>
       {/* <ul className="benefits">{benefitList}</ul> */}
-      <p>{org}</p>
-      <p>{director}</p>
-      <ul className="techTags">{techTags}</ul>
+      <p className="tech__org">
+        Organization: {org}
+      </p>
+      <p className="tech__status">
+        Status: {status}
+      </p>
+      <ul className="tech__techTags">{techTags}</ul>
     </article>
   )
 }
 
+const { string, array } = PropTypes;
 TechnologyContent.propTypes = {
-  data: PropTypes.array
-}
+  discoveryMethod: string, 
+  description: string, 
+  techAreas: array, 
+  director: string, 
+  benefits: array, 
+  status: string,
+  title: string, 
+  org: string
+};
