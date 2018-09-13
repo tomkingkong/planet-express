ab76487fe5aeaa17726a711cfa6649d103303f84
Tom King <design.tomking@gmail.com>
2018-09-12 18:44:48 -0600

    Create a journey route component to render encounters and arrival

---
+++ src/containers/JourneyRoute/index.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import { Encounter } from '../../components/Encounter';
+import { Arrival } from '../../components/Arrival';
+
+export class JourneyRoute extends Component {
+  constructor() {
+    super();
+    this.state = {
+      page: 0
+    }
+  }
+
+  nextEvent = () => {
+    const { page } = this.state;
+    const { journey, history } = this.props;
+    if (page < journey.encounters.length-1) {
+      this.setState({ 
+        page: page + 1 
+      }, history.replace(`/journey/encounter/${this.state.page}`));
+    } else {
+      history.replace('/journey/arrival');
+    }
+  }
+
+  render() {
+    const { exoplanets, journey, spaceEvents } = this.props;
+    const destination = exoplanets
+      .find(planet => planet.name === journey.planetId);
+    const encounter = spaceEvents
+      .find(event => event.id === journey.encounters[this.state.page]);
+    return (
+      <Switch>
+        <Route exact path="/journey/arrival" render={({history}) => <Arrival history={history} destination={destination}/>} />
+        <Route path="/journey" render={() => <Encounter nextEvent={this.nextEvent} {...encounter} />} />
+      </Switch>
+    )
+  }
+}
+
+const { object, array } = PropTypes;
+JourneyRoute.propTypes = {
+  journey: object,
+  exoplanets: array,
+  spaceEvents: array,
+};
+
+export const mapStateToProps = ({ 
+  journey, 
+  exoplanets, 
+  spaceEvents }) => ({
+    journey,
+    exoplanets,
+    spaceEvents
+  });
+
+export default connect(mapStateToProps, null)(JourneyRoute);
