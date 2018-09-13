da9b440d48f895a03062d96f1a02b017eeae9045
Tom King <design.tomking@gmail.com>
2018-09-12 18:42:28 -0600

    Create a prelaunch navigation ui

--- src/components/PreLaunchNav/index.js
+++ src/components/PreLaunchNav/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './PreLaunchNav.css'
+
 export const PreLaunchNav = () => {
   return (
-    <div>
-      <Link to="/Launch">Launch!</Link>
-      <Link to="/News">News</Link>
+    <div className="preLaunchNav">
+      <Link className="launch" to="/Launch">
+        <p>Launch!</p>
+      </Link>
+      <Link className="news" to="/News">
+        <p>News</p>
+      </Link>
     </div>
   )
 }
