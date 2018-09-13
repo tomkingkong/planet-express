58501190d01307e5f5a20ade0a5a8d6415bc79eb
Tom King <design.tomking@gmail.com>
2018-09-12 18:34:00 -0600

    Add back button and proptypes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-export const Footer = () => {
+import './Footer.css';
+
+export const Footer = ({ history }) => {
   return (
-    <div>
-      <Link to='/profile'>( Profile )</Link>
+    <div className="Footer">
+      <button className="footer__back" onClick={() => history.goBack()}>Back</button>
+      <Link className="profile" to='/profile'>Profile</Link>
     </div>
   )
 }
+
+const { object } = PropTypes;
+Footer.propTypes = {
+  history: object
+} 
