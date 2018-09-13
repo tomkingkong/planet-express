c2cabfc1c787a06294e04f2a0e58b80a9f44cb5d
Tom King <design.tomking@gmail.com>
2018-09-12 18:45:13 -0600

    Create a news container to hold all news articles

--- src/containers/NewsContainer/index.js
+++ src/containers/NewsContainer/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+import './NewsSection.css';
 import { NewsContent } from '../../components/NewsContent';
 
 export const NewsContainer = ({ news }) => {
   const displayNews = news.map((info, i) => <NewsContent {...info} key={i}/>)
   return (
     <section className="news_container">
+      <h1 className="news__section__title">Out of this world News!</h1>
       { displayNews }
     </section>
   )
