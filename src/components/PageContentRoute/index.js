162cc923c22934e55c09850c3d173da9664ab6f1
Tom King <design.tomking@gmail.com>
2018-09-12 18:41:47 -0600

    Add routes to pagecontent route

--- src/components/PageContentRoute/index.js
+++ src/components/PageContentRoute/index.js
@@ -4,15 +4,17 @@ import { Route, Switch } from 'react-router-dom';
 import { profile } from '../profile';
 import { Shuttle } from '../../components/Shuttle';
 import { StationContentRoute } from '../StationContentRoute';
+import JourneyRoute from '../../containers/JourneyRoute';
 import NewsContainer from '../../containers/NewsContainer';
 
 export const PageContentRoute = () => {
   return (
     <Switch>
-      <Route exact path='/profile' component={profile} />
       <Route exact path='/launch' component={Shuttle} />
+      <Route exact path='/profile' component={profile} />
       <Route exact path='/news' component={NewsContainer} />
       <Route path='/ISS/*' component={StationContentRoute} />
+      <Route path='/journey' component={JourneyRoute} />
     </Switch>
   )
 }
