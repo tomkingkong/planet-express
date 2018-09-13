5a89e4d28bfaf762758c10438701727a3577c2a3
Tom King <design.tomking@gmail.com>
2018-09-12 18:42:50 -0600

    Create a shuttle component to handle gobetween ground and spacestation

--- src/components/Shuttle/index.js
+++ src/components/Shuttle/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Shuttle.css';
 
 export const Shuttle = ({ history }) => {
+  const imgLaunchStyle = {
+    backgroundImage: "url(/images/shuttle/shuttle_takeoff_smokey.jpg)"
+  }
+  const information = 'Launching from Cape Canaveral on DATE, headed for rendezvous with the Internation Space Station.'
+  const continueToStation = () => {
+    history.replace('/ISS');
+  }
   return (
-    <div>
-      <p>some text n shit</p>
-      <button onClick={() => history.replace('/ISS')}>Continue ></button>
+    <div className="Shuttle">
+      <div className="shuttle__launch_img" style={imgLaunchStyle} />
+      <div className="shuttle__info">
+        <h4 className="shuttle__headline">Departing Earth..</h4>
+        <p className="shuttle__desc">{information}</p>
+      </div>
+      <button className="continue_btn" onClick={continueToStation}>continue</button>
     </div>
   )
 }
 
+
+const { object } = PropTypes;
+Shuttle.propTypes = {
+  history: object
+} 
