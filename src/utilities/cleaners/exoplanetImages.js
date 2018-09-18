acb3e966768c851fb2c445afb589ee9405933a31
Tom King <design.tomking@gmail.com>
2018-09-17 22:08:39 -0600

    Add exoplanet file

---
+++ src/utilities/cleaners/exoplanetImages.js
@@ -0,0 +1,14 @@
+export const exoplanetImages = [
+  'exoplanet_icy_1.png', 
+  'exoplanet_rocky_1.png',
+  'exoplanet_rocky_2.jpg',
+  'exoplanet_rocky_3.jpg',
+  'exoplanet_rocky_4.jpg',
+  'exoplanet_rocky_5.jpg',
+  'exoplanet_habitable_1.png',
+  'exoplanet_habitable_2.png',
+  'exoplanet_Mercury_1.jpg',
+  'exoplanet_Mercury_2.jpg',
+  'exoplanet_Pluto.jpg',
+  'exoplanet_Pluto_2.jpg'
+]
