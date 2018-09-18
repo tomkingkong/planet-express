545d72da939b7df09b52133d072367f3884bfb76
Tom King <design.tomking@gmail.com>
2018-09-17 21:31:23 -0600

    Add action test for spaceEvents

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -7,3 +7,10 @@ import * as actions from '../actions';
       const expected = {type:'POPULATE_EXOPLANETS', exoplanets:mockExoplanets};
       expect(results).toEqual(expected);
     });
+    
+    it('should create an object with an spaceEvents array and type POPULATE_NEWS', () =>{
+      const mockNews = [{ news:1 }, { news:2 }];
+      const results = actions.populateNews(mockNews);
+      const expected = {type:'POPULATE_NEWS', news:mockNews};
+      expect(results).toEqual(expected);
+    });
