424896e3eb98d53b29b6dc662ea9edea3576870f
Tom King <design.tomking@gmail.com>
2018-09-17 21:41:14 -0600

    Add back button test to footer component

--- src/components/Footer/index.test.js
+++ src/components/Footer/index.test.js
@@ -9,3 +9,11 @@ describe('Footer component', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
+  it('should take the user back to their previous page', () => {
+    const mockFn = jest.fn();
+    const history = {goBack:mockFn}
+    const wrapper = shallow(<Footer history={history} />);
+    wrapper.find('.footer__back').simulate('click');
+    expect(mockFn).toHaveBeenCalled();
+  });
+});
