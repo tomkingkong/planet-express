4528128005150407578f8a791000b5138ba176cb
Tom King <design.tomking@gmail.com>
2018-09-17 22:07:20 -0600

    Add catch to initial fetches within thunks

--- src/thunks/index.js
+++ src/thunks/index.js
@@ -17,62 +17,47 @@ import {
 
 export const importExoplanets = () => {
   return async (dispatch) => {
-    try {
-      const exoplanets = await fetchPlanets();
-      const limitPlanets = exoplanets.slice(400, 410);
-      const cleanPlanets = exoplanetCleaner(limitPlanets);
-      dispatch(populateExoplanets(cleanPlanets));
-    } catch (error) {
-      console.log(error);
-    }
+    const exoplanets = await fetchPlanets();
+    if (!exoplanets) return false;
+    const limitPlanets = exoplanets.slice(0, 10);
+    const cleanPlanets = exoplanetCleaner(limitPlanets);
+    dispatch(populateExoplanets(cleanPlanets));
   }
 }
 
 export const importTechnologies = () => {
   return async (dispatch) => {
-    try {
-      const technologies = await fetchTechnologies();
-      const limitTechnologies = technologies.projects.projects.slice(0, 10);
-      const dirtyTechs = await Promise.all(limitTechnologies
-        .map(tech => fetchTechnology(tech.id)));
-
-      const cleanTechs = techCleaner(dirtyTechs);
-      dispatch(populateTechnologies(cleanTechs));
-    } catch (error) {
-      console.log(error);
-    }
+    const technologies = await fetchTechnologies();
+    if (!technologies) return false;
+    const limitTechnologies = technologies.projects.projects.slice(0, 10);
+    const dirtyTechs = await Promise.all(limitTechnologies
+      .map(tech => fetchTechnology(tech.id)));
+    const cleanTechs = techCleaner(dirtyTechs);
+    dispatch(populateTechnologies(cleanTechs));
   }
 }
 
 export const importScienceNews = () => {
   return async (dispatch) => {
-    try {
-      const scienceNews = await fetchScienceNews();
-      const newswithImages = await scienceNews.articles
+    const scienceNews = await fetchScienceNews();
+    if (!scienceNews) return false;
+    const newswithImages = scienceNews.articles
       .filter(article => article.urlToImage);
-
-      const limitNews = newswithImages.slice(3, 9);
-      dispatch(populateNews(limitNews));
-    } catch (error) {
-      console.log(error);
-    }
+    const limitNews = newswithImages.slice(3, 9);
+    dispatch(populateNews(limitNews));
   }
 }
 
 export const importSpaceEvents = () => {
   return async (dispatch) => {
-    try {
-      const fetchEvents = await fetchSpaceEvents();
-      const spaceEvents = await Promise.all(fetchEvents
-        .map( async event => { 
-          const eventData = await fetchSpaceEvent(event.id);
-          return {...eventData, id:event.id}
-        }));
-
-      dispatch(populateSpaceEvents(spaceEvents));
-    } catch (error) {
-      console.log(error);
-    }
+    const fetchEvents = await fetchSpaceEvents();
+    if (!fetchEvents) return false;
+    const spaceEvents = await Promise.all(fetchEvents
+      .map( async event => { 
+        const eventData = await fetchSpaceEvent(event.id);
+        return {...eventData, id:event.id}
+      }));
+    dispatch(populateSpaceEvents(spaceEvents));
   }
 }
 
