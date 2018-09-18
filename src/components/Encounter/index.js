a33165ade4353c5eb93ded673bccc80497cfc7a6
Tom King <design.tomking@gmail.com>
2018-09-17 21:36:43 -0600

    Update render method of encounter component

--- src/components/Encounter/index.js
+++ src/components/Encounter/index.js
@@ -8,7 +8,7 @@ export default class Encounter extends Component {
     super();
     this.state = {
       showInfo: false
-  }
+    }
   }
   handleClick = () => {
     this.setState({ showInfo:!this.state.showInfo })
@@ -16,18 +16,27 @@ export default class Encounter extends Component {
   
   displayInfo = () => {
     const { name, description } = this.props;
-  return (
+    return (
       <div className="encounter__info">
-      <h4 className="encounter__title">{name}</h4>
-      <p className="encounter__desc">{description}</p>
+        <h4 className="encounter__title">{name}</h4>
+        <p className="encounter__desc">{description}</p>
       </div>
     )
   }
-    </article>
-  )
+  
+  render() {
+    const encounterImgStyle = {
+      backgroundImage: `url(${this.props.image_files[2].file_url})`
+    }
+    return (
+      <article className="Encounter"  >
+        <div className="encounter__img" style={encounterImgStyle} onClick={this.handleClick}/>
+        { this.state.showInfo && this.displayInfo() }
+      </article>
+    )
+  }
 }
 
-const { string, array } = PropTypes;
 Encounter.propTypes = {
   name: string,
   description: string,
