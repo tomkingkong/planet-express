9712b75e43aa6ba760af78da9f238b0ecea62df5
Tom King <design.tomking@gmail.com>
2018-09-17 21:42:33 -0600

    Update path route in navroute component

--- src/components/NavRoute/index.js
+++ src/components/NavRoute/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
-import { SpaceStationNav } from '../SpaceStationNav'
+import { SpaceStationNav } from '../SpaceStationNav';
 import { PreLaunchNav } from '../PreLaunchNav';
 import { Footer } from '../Footer';
 
 export const NavRoute = () => {
   return (
     <div>
-      <Route path="/" render={({ location, history }) => (location.pathname !== '/profile') && <Footer history={history}  />} />
+      <Route path='/' render={({ location, history }) => (location.pathname !== '/profile') && <Footer history={history} />} />
       <Route exact path='/' component={PreLaunchNav} />
       <Route exact path='/ISS' component={SpaceStationNav} />
     </div>
