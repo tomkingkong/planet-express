21958061a45eae48a46a0811477405f249509aa9
Tom King <design.tomking@gmail.com>
2018-09-17 21:45:09 -0600

    Add snapshot test to planetcontent

---
+++ src/components/PlanetContent/index.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { PlanetContent } from '.';
+
+describe('PlanetContent component', () => {
+  it('should match snapshot', () => {
+    const mockProps = {
+      name: '', 
+      image: '', 
+      distance: 1, 
+      hostStar: '', 
+      yearLength: 1, 
+      numInSystem: '',
+      discoveryMethod: ''
+    }
+    const wrapper = shallow(
+    <PlanetContent 
+      {...mockProps}
+    />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
