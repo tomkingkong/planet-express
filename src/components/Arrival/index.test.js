626ec953e63d95bc04cdaecab70de1c845e8493d
Tom King <design.tomking@gmail.com>
2018-09-17 21:35:09 -0600

    Add button click test to arrival component

--- src/components/Arrival/index.test.js
+++ src/components/Arrival/index.test.js
@@ -8,3 +8,12 @@ describe('Arrival component', () => {
     const wrapper = shallow(<Arrival />);
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should send the user to the home page on click', () => {
+    const mockFn = jest.fn();
+    const history = {replace:mockFn}
+    const wrapper = shallow(<Arrival history={history} />);
+    wrapper.find('.retire_btn').simulate('click');
+    expect(mockFn).toHaveBeenCalled();
+  });
+});
