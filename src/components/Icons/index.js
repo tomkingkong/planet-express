c3e915203006ac4bdc1074e07e8ffcdf90f81755
Tom King <design.tomking@gmail.com>
2018-09-17 21:41:34 -0600

    Add planetIcon component

---
+++ src/components/Icons/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { string } from 'prop-types';
+
+import './Icon.css';
+
+export const PlanetIcon = ({ planetMap }) => {
+  const planetImageStyle = { backgroundImage: `url(${planetMap})` };
+  return (
+    <div className="planet__wrapper">
+      <div className="planet" style={planetImageStyle} />
+    </div>
+  )
+}
+
+PlanetIcon.propTypes = {
+  planetMap: string
+}
