ee0a647d0633e6a9e3b60bdfeb57f6d90235573a
Tom King <design.tomking@gmail.com>
2018-09-17 21:43:45 -0600

    Add proptypes to newscontent

--- src/components/NewsContent/index.js
+++ src/components/NewsContent/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { string } from 'prop-types';
 
 import './News.css';
 
@@ -25,7 +25,6 @@ export const NewsContent = ({
   )
 }
 
-const { string } = PropTypes;
 NewsContent.propTypes = {
   description: string, 
   publishedAt: string, 
