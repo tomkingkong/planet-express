8e9482518c5c026944c3117f3a1ead6b181ce61c
Tom King <design.tomking@gmail.com>
2018-09-17 21:47:02 -0600

    Update stationcontentroute component routes

--- src/components/StationContentRoute/index.js
+++ src/components/StationContentRoute/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import DestinationsContainer from '../../containers/DestinationsContainer';
+import TravelContainer from '../../containers/TravelContainer';
 import TechnologiesContainer from '../../containers/TechnologiesContainer';
 
 export const StationContentRoute = () => {
   return (
     <Switch>
-      <Route path="/ISS/destinations" component={DestinationsContainer} />
+      <Route path="/ISS/destinations" component={TravelContainer} />
       <Route path="/ISS/store" component={TechnologiesContainer} />
     </Switch>
   )
