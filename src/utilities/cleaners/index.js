931641f4385931cbd5e194218dd6add7f8502992
Tom King <design.tomking@gmail.com>
2018-09-17 22:08:54 -0600

    Update cleaner file

--- src/utilities/cleaners/index.js
+++ src/utilities/cleaners/index.js
@@ -1,22 +1,17 @@
 import $ from "jquery";
 
-export const exoplanetCleaner = (planets) => {
-  const planetImages = [
-    'exoplanet_blue_1.png', 
-    'exoplanet_blue_2.jpg', 
-    'exoplanet_icy_1.png', 
-    'exoplanet_rocky_1.png',
-    'exoplanet_rocky_2.jpg'
-  ]
+import { exoplanetImages } from './exoplanetImages';
 
+export const exoplanetCleaner = (planets) => {
+  
   return planets.map(planet => {
     let numInSystem = planet.pl_pnum;
-    if (numInSystem > 4) { numInSystem = numInSystem+'th' } else
+    if (numInSystem >= 4) { numInSystem = numInSystem+'th' } else
     if (numInSystem === 3) { numInSystem = numInSystem+'rd' } else
     if (numInSystem === 2) { numInSystem = numInSystem+'nd' } else
     if (numInSystem === 1) { numInSystem = numInSystem+'st' }
     
-    const planetImage = planetImages[(Math.random()*planetImages.length-1)+0.5<<0];
+    const planetImage = exoplanetImages[(Math.random()*exoplanetImages.length-1)+0.5<<0];
     return {
       name: planet.pl_name,
       mass: planet.pl_bmassj,
@@ -35,7 +30,7 @@ export const exoplanetCleaner = (planets) => {
 export const techCleaner = (technologies) => {
   return technologies.map(tech => {
     const { project } = tech;
-    const technologyAreas = project.primaryTas.technologyAreas || [];
+    const technologyAreas = [...project.primaryTas.technologyAreas || []];
     const techTags = technologyAreas.map(tech => tech.name.split(/(\band\b,)+g/));
     return {
       title: project.title,
@@ -44,13 +39,11 @@ export const techCleaner = (technologies) => {
       discoveryMethod: project.pl_discmethod || 'unknown',
       techAreas: [].concat($.unique([techTags.join(',')])),
       // director: project.programDirectors.programDirector || 'unknown',
-      benefits: $.parseHTML(project.benefits)[0].innerText,
+      // benefits: $.parseHTML(project.benefits)[0].innerText,
       description: $.parseHTML(project.description)[0].innerText
     }
   })
 }
-
-
 
 //breakdown techAreas into tags
 //clean html code, and split benefits
