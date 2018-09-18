42de41c90610b94244b56a3867cf61f090db3804
Tom King <design.tomking@gmail.com>
2018-09-17 22:12:30 -0600

    Add mockdata file

---
+++ src/utilities/mockData/mockData.js
@@ -0,0 +1,380 @@
+export const newsArray = {articles:[
+  {
+    author : 'poo',
+    content : "In August of 2016, astronomers from the European Southern Observatory (ESO) confirmed the existence of an Earth-like planet around Proxima Centauri – the closest star to our solar system. In addition, they confirmed that this planet (Proxima b) orbited within… [+8792 chars]",
+    description : "In August of 2016, astronomers from the European Southern Observatory (ESO) confirmed the existence of an Earth-like planet around Proxima Centauri – the closest star to our solar system. In addition, they confirmed that this planet (Proxima b) orbited within…",
+    publishedAt : "2018-09-17T14:07:21Z",
+    source : {id: null, name: "Phys.org"},
+    title : "Closest planet ever discovered outside solar system could be habitable with a dayside ocean",
+    url : "https://phys.org/news/2018-09-closest-planet-solar-habitable-dayside.html",
+    urlToImage : "https://3c1703fe8d.site.internapcdn.net/newman/gfx/news/2018/theclosestpl.jpg"
+  }
+]}
+
+export const exoplanets = [
+  {
+    dec : -3.560602,
+    dec_str : "-03d33m38.2s",
+    gaia_gmag : 6.621,
+    gaia_gmagerr : null,
+    gaia_gmaglim : 0,
+    pl_bmassj : 0.26,
+    pl_bmassjerr1 : 0.02,
+    pl_bmassjerr2 : -0.02,
+    pl_bmassjlim : 0,
+    pl_bmassn : 2,
+    pl_bmassprov : "Msini",
+    pl_dens : null,
+    pl_denserr1 : null,
+    pl_denserr2 : null,
+    pl_denslim : 0,
+    pl_densn : 0,
+    pl_discmethod : "Radial Velocity",
+    pl_facility : "W. M. Keck Observatory",
+    pl_hostname : "HD 16141",
+    pl_k2flag : 0,
+    pl_kepflag : 0,
+    pl_letter : "b",
+    pl_name : "HD 16141 b",
+    pl_nnotes : 0,
+    pl_orbeccen : 0.25,
+    pl_orbeccenerr1 : 0.05,
+    pl_orbeccenerr2 : -0.05,
+    pl_orbeccenlim : 0,
+    pl_orbeccenn : 2,
+    pl_orbincl : null,
+    pl_orbinclerr1 : null,
+    pl_orbinclerr2 : null,
+    pl_orbincllim : 0,
+    pl_orbincln : 0,
+    pl_orbper : 75.523,
+    pl_orbpererr1 : 0.055,
+    pl_orbpererr2 : -0.055,
+    pl_orbperlim : 0,
+    pl_orbpern : 2,
+    pl_orbsmax : 0.36,
+    pl_orbsmaxerr1 : 0.01,
+    pl_orbsmaxerr2 : -0.01,
+    pl_orbsmaxlim : 0,
+    pl_orbsmaxn : 2,
+    pl_pnum : 1,
+    pl_radj : null,
+    pl_radjerr1 : null,
+    pl_radjerr2 : null,
+    pl_radjlim : 0,
+    pl_radn : 0,
+    pl_ttvflag : 0,
+    ra : 38.833035,
+    ra_str : "02h35m19.93s",
+    rowupdate : "2018-09-06",
+    st_decerr : 0.000002,
+    st_dist : 35.91,
+    st_disterr1 : 1.71,
+    st_disterr2 : -1.89,
+    st_distlim : 0,
+    st_distn : 4,
+    st_mass : 1.11,
+    st_masserr1 : 0.06,
+    st_masserr2 : -0.06,
+    st_masslim : 0,
+    st_massn : 7,
+    st_optband : "V (Johnson)",
+    st_optmag : 6.83,
+    st_optmagerr : null,
+    st_optmaglim : 0,
+    st_posn : 2,
+    st_rad : 1.48,
+    st_raderr1 : 0.02,
+    st_raderr2 : -0.02,
+    st_radlim : 0,
+    st_radn : 4,
+    st_raerr : 0.000003,
+    st_teff : 5757,
+    st_tefferr1 : 9,
+    st_tefferr2 : -9,
+    st_tefflim : 0,
+    st_teffn : 8
+  },
+  {
+    dec : -3.560602,
+    dec_str : "-03d33m38.2s",
+    gaia_gmag : 6.621,
+    gaia_gmagerr : null,
+    gaia_gmaglim : 0,
+    pl_bmassj : 0.26,
+    pl_bmassjerr1 : 0.02,
+    pl_bmassjerr2 : -0.02,
+    pl_bmassjlim : 0,
+    pl_bmassn : 2,
+    pl_bmassprov : "Msini",
+    pl_dens : null,
+    pl_denserr1 : null,
+    pl_denserr2 : null,
+    pl_denslim : 0,
+    pl_densn : 0,
+    pl_discmethod : "Radial Velocity",
+    pl_facility : "W. M. Keck Observatory",
+    pl_hostname : "HD 16141",
+    pl_k2flag : 0,
+    pl_kepflag : 0,
+    pl_letter : "b",
+    pl_name : "HD 16141 c",
+    pl_nnotes : 0,
+    pl_orbeccen : 0.25,
+    pl_orbeccenerr1 : 0.05,
+    pl_orbeccenerr2 : -0.05,
+    pl_orbeccenlim : 0,
+    pl_orbeccenn : 2,
+    pl_orbincl : null,
+    pl_orbinclerr1 : null,
+    pl_orbinclerr2 : null,
+    pl_orbincllim : 0,
+    pl_orbincln : 0,
+    pl_orbper : 75.523,
+    pl_orbpererr1 : 0.055,
+    pl_orbpererr2 : -0.055,
+    pl_orbperlim : 0,
+    pl_orbpern : 2,
+    pl_orbsmax : 0.36,
+    pl_orbsmaxerr1 : 0.01,
+    pl_orbsmaxerr2 : -0.01,
+    pl_orbsmaxlim : 0,
+    pl_orbsmaxn : 2,
+    pl_pnum : 1,
+    pl_radj : null,
+    pl_radjerr1 : null,
+    pl_radjerr2 : null,
+    pl_radjlim : 0,
+    pl_radn : 0,
+    pl_ttvflag : 0,
+    ra : 38.833035,
+    ra_str : "02h35m19.93s",
+    rowupdate : "2018-09-06",
+    st_decerr : 0.000002,
+    st_dist : 35.91,
+    st_disterr1 : 1.71,
+    st_disterr2 : -1.89,
+    st_distlim : 0,
+    st_distn : 4,
+    st_mass : 1.11,
+    st_masserr1 : 0.06,
+    st_masserr2 : -0.06,
+    st_masslim : 0,
+    st_massn : 7,
+    st_optband : "V (Johnson)",
+    st_optmag : 6.83,
+    st_optmagerr : null,
+    st_optmaglim : 0,
+    st_posn : 2,
+    st_rad : 1.48,
+    st_raderr1 : 0.02,
+    st_raderr2 : -0.02,
+    st_radlim : 0,
+    st_radn : 4,
+    st_raerr : 0.000003,
+    st_teff : 5757,
+    st_tefferr1 : 9,
+    st_tefferr2 : -9,
+    st_tefflim : 0,
+    st_teffn : 8
+  },
+  {
+    dec : -3.560602,
+    dec_str : "-03d33m38.2s",
+    gaia_gmag : 6.621,
+    gaia_gmagerr : null,
+    gaia_gmaglim : 0,
+    pl_bmassj : 0.26,
+    pl_bmassjerr1 : 0.02,
+    pl_bmassjerr2 : -0.02,
+    pl_bmassjlim : 0,
+    pl_bmassn : 2,
+    pl_bmassprov : "Msini",
+    pl_dens : null,
+    pl_denserr1 : null,
+    pl_denserr2 : null,
+    pl_denslim : 0,
+    pl_densn : 0,
+    pl_discmethod : "Radial Velocity",
+    pl_facility : "W. M. Keck Observatory",
+    pl_hostname : "HD 16141",
+    pl_k2flag : 0,
+    pl_kepflag : 0,
+    pl_letter : "b",
+    pl_name : "HD 16141 a",
+    pl_nnotes : 0,
+    pl_orbeccen : 0.25,
+    pl_orbeccenerr1 : 0.05,
+    pl_orbeccenerr2 : -0.05,
+    pl_orbeccenlim : 0,
+    pl_orbeccenn : 2,
+    pl_orbincl : null,
+    pl_orbinclerr1 : null,
+    pl_orbinclerr2 : null,
+    pl_orbincllim : 0,
+    pl_orbincln : 0,
+    pl_orbper : 75.523,
+    pl_orbpererr1 : 0.055,
+    pl_orbpererr2 : -0.055,
+    pl_orbperlim : 0,
+    pl_orbpern : 2,
+    pl_orbsmax : 0.36,
+    pl_orbsmaxerr1 : 0.01,
+    pl_orbsmaxerr2 : -0.01,
+    pl_orbsmaxlim : 0,
+    pl_orbsmaxn : 2,
+    pl_pnum : 1,
+    pl_radj : null,
+    pl_radjerr1 : null,
+    pl_radjerr2 : null,
+    pl_radjlim : 0,
+    pl_radn : 0,
+    pl_ttvflag : 0,
+    ra : 38.833035,
+    ra_str : "02h35m19.93s",
+    rowupdate : "2018-09-06",
+    st_decerr : 0.000002,
+    st_dist : 35.91,
+    st_disterr1 : 1.71,
+    st_disterr2 : -1.89,
+    st_distlim : 0,
+    st_distn : 4,
+    st_mass : 1.11,
+    st_masserr1 : 0.06,
+    st_masserr2 : -0.06,
+    st_masslim : 0,
+    st_massn : 7,
+    st_optband : "V (Johnson)",
+    st_optmag : 6.83,
+    st_optmagerr : null,
+    st_optmaglim : 0,
+    st_posn : 2,
+    st_rad : 1.48,
+    st_raderr1 : 0.02,
+    st_raderr2 : -0.02,
+    st_radlim : 0,
+    st_radn : 4,
+    st_raerr : 0.000003,
+    st_teff : 5757,
+    st_tefferr1 : 9,
+    st_tefferr2 : -9,
+    st_tefflim : 0,
+    st_teffn : 8
+  }
+]
+
+export const spaceEvents = [
+  {
+    collection : "news",
+    id : 4229,
+    name : "Galaxy Cluster Abell 370",
+    news_name : "a"
+  }
+]
+
+export const mappedSpaceEvent = {
+  collection : "news",
+  description : "This image shows a massive galaxy cluster embedded in the middle of a field of nearly 8,000 galaxies",
+  id : 4229,
+  image_files : [
+  { file_url: "https://media.stsci.edu/uploads/image_file/image_attachment/30945/STSCI-H-p1839a-z-878x1000.png", file_size: 1371215, width: 878, height: 1000 },
+  { file_url: "https://media.stsci.edu/uploads/image_file/image_attachment/30944/STSCI-H-p1839a-m-1757x2000.png", file_size: 5663943, width: 1757, height: 2000 },
+  { file_url: "https://media.stsci.edu/uploads/image_file/image_attachment/30943/STSCI-H-p1839a-f-7819x8897.png", file_size: 93209692, width: 7819, height: 8897 }
+  ],
+  mission : "hubble",
+  name : "Galaxy Cluster Abell 370",
+  news_name : "a"
+}
+
+export const technologyIds = {projects: {projects: [
+  { id : 17389, lastUpdated : "2018-9-11" }
+]}}
+
+export const technology = {
+  project: {
+    additionalTas : "",
+    benefits : "<p>This project will benefit Earth Science Missions like NASA&#39;s planned ASCENDS Mission.</p><p>The technology will also benefit future&nbsp;orbital missions&nbsp;to measure the properties of the atmosphere&nbsp;using&nbsp;lidar.</p>",
+    coFundingPartners : {
+      organization: {
+        city : "Somerset",
+        name : "OFS Laboratories",
+        state : "NY",
+        type : "Industry"
+      }
+    },
+    coInvestigators : {
+      coInvestigator:[]
+    },
+    description : "<p>We have been developing the CO2 Sounder lidar to provide active (laser-based) column measurements of atmospheric CO2 from space. &nbsp;The tasks are to conduct OSSE experiments to clarify the science traceability matrix, support&nbsp;laser development to increase the laser power and increase the TRL,&nbsp; improving the receiver design and analysis and retrieval approaches, and to analyze lidar measurements of CO2 made from aircraft.</p>",
+    destinations : {
+      destination: []
+    },
+    endDate : "Sep 2018",
+    id : 17389,
+    lastUpdated : "2018-09-11",
+    leadOrganization : {acronym: "GSFC", city: "Greenbelt", name: "Goddard Space Flight Center", state: "MD", type: "NASA Center"},
+    library : {libraryItem: []},
+    primaryTas : {
+      technologyAreas: [
+        {
+          code : 8,
+          id : 3246,
+          name : "Science Instruments, Observatories, and Sensor Systems"
+        }
+      ]
+    },
+    principalInvestigators : {
+      principalInvestigator: "James B Abshire"
+    },
+    projectManagers : {
+      projectManager: [
+        "Matthew Mcgill",
+        "William E Cutlip"
+      ]
+    },
+    responsibleMissionDirectorateOrOffice : "Mission Support Directorate",
+    responsibleProgram : "Center Independent Research & Development: GSFC IRAD",
+    startDate : "Oct 2014",
+    status : "Active",
+    supportedMissionType : "Planned Mission (Pull)",
+    technologyMaturityCurrent : 4,
+    technologyMaturityEnd : 6,
+    technologyMaturityStart : 3,
+    title : "CO2 Sounder Lidar for Earth Science Missions",
+    website : "http://sciences.gsfc.nasa.gov/sed/"
+  }
+}
+
+export const actionTechs = [{
+  "description": undefined, 
+  "discoveryMethod": "unknown", 
+  "org": "Goddard Space Flight Center", 
+  "status": "Active", 
+  "techAreas": [ "Science Instruments, Observatories, and Sensor Systems" ], 
+  "title": "CO2 Sounder Lidar for Earth Science Missions"
+}]
+
+export const actionSpaceEvents = [
+  {
+    "collection": "news", 
+    "description": "This image shows a massive galaxy cluster embedded in the middle of a field of nearly 8,000 galaxies", 
+    "id": 4229, 
+    "image_files": [
+      {"file_size": 1371215, "file_url": "https://media.stsci.edu/uploads/image_file/image_attachment/30945/STSCI-H-p1839a-z-878x1000.png", "height": 1000, "width": 878}, 
+      {"file_size": 5663943, "file_url": "https://media.stsci.edu/uploads/image_file/image_attachment/30944/STSCI-H-p1839a-m-1757x2000.png", "height": 2000, "width": 1757}, 
+      {"file_size": 93209692, "file_url": "https://media.stsci.edu/uploads/image_file/image_attachment/30943/STSCI-H-p1839a-f-7819x8897.png", "height": 8897, "width": 7819}], 
+    "mission": "hubble", 
+    "name": "Galaxy Cluster Abell 370", 
+    "news_name": "a"
+  }
+]
+
+export const actionExoplanets = [
+  {"discoveryBy": "W. M. Keck Observatory", "discoveryMethod": "Radial Velocity", "distance": 35.91, "hostStar": "HD 16141", "image": "/images/alternative/exoplanet_icy_1.png", "mass": 0.26, "name": "HD 16141 b", "numInSystem": "1st", "numMoons": 0, "yearLength": 75.523}, 
+  {"discoveryBy": "W. M. Keck Observatory", "discoveryMethod": "Radial Velocity", "distance": 35.91, "hostStar": "HD 16141", "image": "/images/alternative/exoplanet_icy_1.png", "mass": 0.26, "name": "HD 16141 c", "numInSystem": "1st", "numMoons": 0, "yearLength": 75.523}, 
+  {"discoveryBy": "W. M. Keck Observatory", "discoveryMethod": "Radial Velocity", "distance": 35.91, "hostStar": "HD 16141", "image": "/images/alternative/exoplanet_icy_1.png", "mass": 0.26, "name": "HD 16141 a", "numInSystem": "1st", "numMoons": 0, "yearLength": 75.523}
+]
+
+export const actionsNews = [
+
+]
