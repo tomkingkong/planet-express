20e31531fa21a8b7d1f54ebd2cb25bf6b5e5d30c
Tom King <design.tomking@gmail.com>
2018-09-17 22:02:19 -0600

    Add travelcontainer container

---
+++ src/containers/TravelContainer/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { array } from 'prop-types';
+import { connect } from 'react-redux';
+
+import { handleScroll } from '../../utilities/ScrollAnimate';
+import './Travel.css';
+import DestinationCard from '../DestinationCard';
+
+export class TravelContainer extends Component {
+  displayExoplanets = () => {
+    const { exoplanets, history } = this.props;
+    return exoplanets.map((planet, i) => (
+      <DestinationCard 
+        key={i} 
+        planet={planet} 
+        history={history} 
+      />
+    ));
+  }
+
+  render() {
+    handleScroll('.travel__locations', '.travel__title');
+    return (
+      <div className="Travel">
+        <header className="travel__title">
+          <h4>Available Destinations</h4>
+        </header>
+        <section className="travel__locations">
+        { this.displayExoplanets() }
+        </section>
+      </div>
+    )
+  }
+}
+
+TravelContainer.propTypes = {
+  exoplanets: array
+};
+
+export const mapStateToProps = ({ exoplanets }) => ({ exoplanets });
+
+export default connect(mapStateToProps, null)(TravelContainer);
