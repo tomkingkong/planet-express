98f6d79263e96729db22cbc8484c094391f91724
Tom King <design.tomking@gmail.com>
2018-09-22 09:15:27 -0600

    Add date to shuttle component

--- src/components/Shuttle/index.js
+++ src/components/Shuttle/index.js
@@ -7,7 +7,10 @@ export const Shuttle = ({ history }) => {
   const imgLaunchStyle = {
     backgroundImage: "url(/images/shuttle/shuttle_takeoff_smokey.jpg)"
   };
-  const information = 'Launching from Cape Canaveral on DATE, headed for rendezvous with the Internation Space Station.';
+  const time = new Date();
+  const information = `Launching from Cape Canaveral on 
+  ${time.constructor().split(' ').slice(0, 4).join(' ')}, 
+  headed for rendezvous with the Internation Space Station.`;
   const continueToStation = () => {
     history.replace('/ISS');
   };
@@ -18,7 +21,10 @@ export const Shuttle = ({ history }) => {
         <h4 className="shuttle__headline">Departing Earth..</h4>
         <p className="shuttle__desc">{information}</p>
       </div>
-      <button className="continue_btn" onClick={continueToStation}>continue</button>
+      <button className="continue_btn" 
+        onClick={continueToStation}>
+        continue
+      </button>
     </div>
   );
 };
