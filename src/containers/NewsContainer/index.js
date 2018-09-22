1d5f94a626dedcd5456fcd43ae233a631e104a9d
Tom King <design.tomking@gmail.com>
2018-09-22 09:18:39 -0600

    Update fonts and touchup styling to newscontainer
    Add handleScroll function

--- src/containers/NewsContainer/index.js
+++ src/containers/NewsContainer/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { array } from 'prop-types';
 import { connect } from 'react-redux';
 
+import { handleScroll } from '../../utilities/ScrollAnimate'
 import './NewsSection.css';
 import { NewsContent } from '../../components/NewsContent';
 
 export const NewsContainer = ({ news }) => {
   const displayNews = news.map((info, i) => <NewsContent {...info} key={i}/>);
+  handleScroll('.news_container', '.news__section__title');
   return (
     <section className="news_container">
-      <h1 className="news__section__title">Out of this world News!</h1>
+      <h1 className="news__section__title">SpaceNews</h1>
       { displayNews }
     </section>
   );
