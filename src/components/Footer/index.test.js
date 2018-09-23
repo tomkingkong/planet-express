d22d236e79d5ad0d0c1f00c78549e0a782e5e302
Tom King <design.tomking@gmail.com>
2018-09-23 08:41:57 -0600

    Fix footer tests to include history location
    Add test to footer component for back button

--- src/components/Footer/index.test.js
+++ src/components/Footer/index.test.js
@@ -5,13 +5,22 @@ import { Footer } from '.';
 
 describe('Footer component', () => {
   it('should match snapshot', () => {
-    const wrapper = shallow(<Footer />);
+    const mockFn = jest.fn();
+    const history = {goBack:mockFn, location: {pathname: '/'}};
+    const wrapper = shallow(<Footer history={history} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should match snapshot with backbutton', () => {
+    const mockFn = jest.fn();
+    const history = {goBack:mockFn, location: {pathname: '/ISS'}};
+    const wrapper = shallow(<Footer history={history} />);
     expect(wrapper).toMatchSnapshot();
   });
 
   it('should take the user back to their previous page', () => {
     const mockFn = jest.fn();
-    const history = {goBack:mockFn};
+    const history = {goBack:mockFn, location: {pathname: '/ISS'}};
     const wrapper = shallow(<Footer history={history} />);
     wrapper.find('.footer__back').simulate('click');
     expect(mockFn).toHaveBeenCalled();
